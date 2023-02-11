f964feb2551aec6de0e02cfc20c5c52c1a84ec20
Ivan <ivantrofim1992@gmail.com>
2023-02-11 19:52:30 +0300

    added task004

--- task04/Program.cs
+++ task04/Program.cs
@@ -3,7 +3,6 @@
 // 2, 3, 7 -> 7
 // 44 5 78 -> 78
 // 22 3 9 -> 22
-int max = 0;
 
 Console.Write("Введите первое число ");
 int number1 = Convert.ToInt32(Console.ReadLine());
@@ -12,10 +11,8 @@ int number2 = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите третье число ");
 int number3 = Convert.ToInt32(Console.ReadLine());
 
-if (number1 > max)
-{
-    max = number1;
-}
+int max = number1;
+
 if (number2 > max)
 {
     max = number2;
@@ -24,4 +21,4 @@ if (number3 > max)
 {
     max = number3;
 }
-Console.WriteLine("max = " + max);
+Console.WriteLine(max);
