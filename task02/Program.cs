08275d41f307739cc16a7b4571e4988004385ae4
Ivan <ivantrofim1992@gmail.com>
2023-02-15 15:57:28 +0300

    added task 13

--- task02/Program.cs
+++ task02/Program.cs
@@ -5,12 +5,12 @@
 // a = -9 b = -3 -> max = -3
 
 Console.Write("Введите первое число ");
-int number1 = Convert.ToInt32(Console.ReadLine()); 
+int number1 = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите второе число ");
-int number2 = Convert.ToInt32(Console.ReadLine()); 
+int number2 = Convert.ToInt32(Console.ReadLine());
 
-if (number2>number1)
-{  
+if (number2 > number1)
+{
     Console.WriteLine($"max={number2}");
 }
 else
---
+++ task13/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+// 645 -> 5
+
+// 78 -> третьей цифры нет
+
+// 32679 -> 6
+
+Console.WriteLine("Введите любое число от 1 до 100000");
+int number = Convert.ToInt32(Console.ReadLine());
+string num = Convert.ToString(number);
+if (num.Length > 2)
+{
+    Console.WriteLine("Третья цифра -> " + num[2]);
+}
+else
+{
+    Console.WriteLine("-> Третьей цифры нет");
+}
